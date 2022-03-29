d35937c0970e9de1c310e3d070f6100e30c4b7cc
alessiav8 <77933342+alessiav8@users.noreply.github.com>
2022-03-29 12:45:39 +0200

    bho

--- BeeTeam/BeeTeam/js/functions.js
+++ BeeTeam/BeeTeam/js/functions.js
@@ -1,9 +1,24 @@
 function edit(){
     
 }
-
+function modpw(){
+    var pass=document.getElementById("pw").ariaValueNow;
+    if(pass.length < 6 ){
+        alert("La password deve contenere almeno 6 caratteri");
+    }
+};
 function Registrazione(){
-   
-    windows.alert("Grazie per la registrazione!");
-   
-}
+    if(isNan(document.getElementById("numero"))){
+        alert("Il numero di telefono non può contenere lettere");
+        window.open("accesso.html");
+        return false;
+    }
+    var pass=document.getElementById("pw").ariaValueNow;
+    if(pass.length < 2 ){
+        alert("La password deve contenere almeno 6 caratteri");
+    }
+    window.open("accesso.html");
+     alert("Grazie per la registrazione!");
+     return true;
+  
+};
