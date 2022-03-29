247f9fe0ea96ffb78d4db62e9c07b03d774e6989
alessiav8 <77933342+alessiav8@users.noreply.github.com>
2022-03-29 19:43:43 +0200

    Accesso ora possibile
    Connessione a firebase per l'autenticazione, inserita all'interno del modulo html, da spostare

---
+++ BeeTeam/BeeTeam/js/functions.mjs
@@ -0,0 +1,14 @@
+/*function edit(){
+    
+}
+function modpw(){
+    var pass=document.getElementById("pw").ariaValueNow;
+    if(pass.length < 6 ){
+        alert("La password deve contenere almeno 6 caratteri");
+    }
+};
+function Registrazione(){
+   
+    windows.alert("Grazie per la registrazione!");
+   
+}*/
