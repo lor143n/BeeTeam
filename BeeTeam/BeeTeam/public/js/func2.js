eb75054a97778f2fe7c9ace9ed70c0cee1a42b64
Alessia Volpi <77933342+alessiav8@users.noreply.github.com>
2022-04-05 18:03:18 +0200

    WORKSSSSS!

--- BeeTeam/BeeTeam/public/js/func2.js
+++ BeeTeam/BeeTeam/public/js/func2.js
@@ -1,14 +1,1 @@
 
-import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
-import { getAuth, signOut} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
-
-var initialize=false;
-
-export function openProfile(){
-    alert("ok");
-        alert("Connect!");
-        window.location.href="profilo.html";
-        let c1= document.getElementById("c1");
-        c1.innerHTML=" <b> CIAO </b>";
-        initialize=false;
-}
