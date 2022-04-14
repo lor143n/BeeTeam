11442030a3cf01a1d47670449231ff11358360f6
Alessia Volpi <77933342+alessiav8@users.noreply.github.com>
2022-04-14 23:40:11 +0200

    Post su profilo

--- BeeTeam/BeeTeam/public/js/functions.js
+++ BeeTeam/BeeTeam/public/js/functions.js
@@ -1,5 +1,13 @@
 
-export function att(id,parent,c,categoria,numero_persone,check, user){
+ export function createElem(id,text,parent){
+    var cat=document.createElement("div");
+    cat.setAttribute("class","row-mb-1");
+    cat.setAttribute("id","cat"+id);
+    cat.innerHTML=text;
+    parent.appendChild(cat);
+    }
+
+export function att(id,parent,c,categoria,numero_persone,check,descrizione, user){
     c=document.createElement("div");
     c.setAttribute("id",id);
     c.setAttribute("class","box-post-bacheca");
@@ -7,7 +15,7 @@ export function att(id,parent,c,categoria,numero_persone,check, user){
     var user_space=document.createElement("div");
     user_space.setAttribute("class","row");
     user_space.setAttribute("style","padding-bottom: 0.5px");
-    if(!check) user_space.innerHTML="<b>User: </b>"+user;
+    if(!check) user_space.innerHTML="<b> User: </b>"+user;
     else user_space.innerHTML="<b> Anonymous </b><br>";
     c.appendChild(user_space);
 
@@ -18,7 +26,7 @@ export function att(id,parent,c,categoria,numero_persone,check, user){
     c.appendChild(space);
     createElem("nump"+id, "<b>Numero di membri richiesti: </b>" + numero_persone,c);
     c.appendChild(space);
-    createElem("descrizione"+id, "<b>Descrizione: </b><br>"+ description.value, c);
+    createElem("descrizione"+id, "<b>Descrizione: </b><br>"+ descrizione, c);
     //createElem("disp"+id, "<b>Posizioni disponibili: </b>" + numero_persone,c);
     c.appendChild(space);
     parent.appendChild(c);
